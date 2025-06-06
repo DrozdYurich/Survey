26be3e436e9344b6183d5be3bf683a15753e732c
Drozdov Evgeny <drozdove912@gmail.com>
2025-06-06 20:21:17 +0300

    Initial

--- apps/client-app/src/app/App.vue
+++ apps/client-app/src/app/App.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts">
-import NxWelcome from './NxWelcome.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <NxWelcome title="@survey/client-app" />
+  <h1>Client-app</h1>
 </template>
