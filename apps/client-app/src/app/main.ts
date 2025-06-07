4a034bfe9cd96d63630bfbec6496aa2702b0ccd1
Drozdov Evgeny <drozdove912@gmail.com>
2025-06-07 09:57:04 +0200

    fsd for client-app

--- apps/client-app/src/app/main.ts
+++ apps/client-app/src/app/main.ts
@@ -1,7 +1,7 @@
-import './styles.css';
+import './styles/styles.css';
 import router from './router';
-import { createApp } from 'vue';
-import App from './app/App.vue';
+import { createApp} from 'vue';
+import App from './App.vue';
 
 const app = createApp(App);
 app.use(router);
